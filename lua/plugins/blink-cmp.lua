579a0e8cc7ff033c8fa56be4f12ee70b86f45c36
luracore <luccarcr@gmail.com>
2026-09-22 16:41:06 -0300

    Atalhos manuais

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,7 +4,18 @@ return {
   version = '1.*',
 
   opts = {
-    keymap = { preset = 'super-tab' },
+    keymap = {
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<CR>'] = { 'accept', 'fallback' },
+
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<Tab>'] = { 'select_next', 'fallback_to_mappings' },
+
+      ['<C-k'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-j>'] = { 'scroll_documentation_down', 'fallback' },
+    },
 
     appearance = {
       nerd_font_variant = "mono"
